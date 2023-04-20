e08e76c9a53b1ee4c3163cf52a913be621299a5e
lvl7558 <lvl7558@g.rit.edu>
2023-04-20 20:17:52 +0200

    Adding quiz and tutorail no style yet

--- Techaro_Group_Project/tutorial.php
+++ Techaro_Group_Project/tutorial.php
@@ -10,9 +10,41 @@
             <ol>
                 <li>First make sure you have a specific type of server you want to upload to. This can be a free online version like AWS. Just make sure it is a reliable web hosting company.</li>
                 <li>Next you can connect to the server by using the ssh or sftp command and connecting to the server</li>
+                <pre><code class="language-bash">
+                    # Connecting to your server
+
+                    $ ssh username@servername
+
+                    #server would then ask if it is your first time to add the server o 
+                    #your list of known hosts
+
+                    #then enter in your own password
+
+                </code></pre> </div>
                 <li>The next step is to make sure you are in the correct directory in order to properly put your website on the web. This can be done using the ls commands and cd command.</li>
+                <pre><code class="language-bash">
+                    # looking in your server
+
+                    $ ls
+                    #some folders and files 
+                    #might need to change directory
+                    $ cd
+                </code></pre> </div>
+                
                 <li>Once in the correct directory, copy your directory or files to the correct space. This can be done using either the scp or sftp commands. </li>
+                <pre><code class="language-bash">
+                    # adding your content to the server can be a file or directory
+
+                    scp localfile.txt username@servername:/remote/directory
+
+                </code></pre> </div>
                 <li>Once done your files should be accessible online. Make sure to check the permissions on the files and directories to allow those to access your information</li>
+                <pre><code class="language-bash">
+                    # chceking if everything has the write permisions 
+
+                    $ ls -l
+
+                </code></pre> </div>
             </ol>
         </div>
 <?php
